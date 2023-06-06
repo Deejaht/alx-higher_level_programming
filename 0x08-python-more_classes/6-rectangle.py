872d77ba6c8bd79b6d1179d616ad23c201660fcb
Deejaht <ravenroth486@gmail.com>
2023-06-06 06:05:16 +0100

    update

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -71,6 +71,7 @@ class Rectangle:
         self.number_of_instances = self.number_of_instances - 1
         print("Bye rectangle...")
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """returns the larger rectangle"""
         if isinstance(rect_1, Rectangle) is False:
