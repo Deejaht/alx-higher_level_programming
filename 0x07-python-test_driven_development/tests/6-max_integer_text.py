175ba77ee78676bfa05d32de9c730e5e39f32d36
Deejaht <ravenroth486@gmail.com>
2023-06-07 06:03:38 +0100

    update

--- 0x07-python-test_driven_development/tests/6-max_integer_text.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_text.py
@@ -8,44 +8,49 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     """Test class"""
 
-    def test_module_docstring(self):
+    def test_one(self):
         """Tests one"""
         m = ('6-max_integer').__doc__
         self.assertTrue(len(m) >= 1)
 
-    def test_function_docstring(self):
+    def test_two(self):
         """Test two"""
         m = max_integer.__doc__
         self.assertTrue(len(m) >= 1)
 
-    def test_max_integer(self):
-        """correct test
+    def test_three(self):
+        """a test
         """
         result = max_integer([10, 18, 13, 12])
         self.assertEqual(18, result)
 
-    def test_two_max_integers(self):
-        """correct test
+    def test_four(self):
+        """a test
         """
         result = max_integer([1, 4, 15, 3, 2, 15])
         self.assertEqual(15, result)
 
-    def test_list_is_empty(self):
-        """List is empty
+    def test_five(self):
+        """a test
         """
         result = max_integer([])
         self.assertEqual(None, result)
 
-    def test_list_param_contains_only_one_arg(self):
-        """List has more"""
+    def test_six(self):
+        """a test"""
         self.assertRaises(TypeError,
-                          max_integer, 1, "2ksdjkf")
+                          max_integer, 1, "2")
 
-
-    def test_not_supported(self):
-        """List param not supported type
+    def test_seven(self):
+        """a test
         """
-        self.assertRaisesRegex(TypeError, "object of type *", max_integer, 1)
+        self.assertRaises(TypeError,
+                          max_integer, [1, 32], [23, 84])
+
+    def test_eight(self):
+        """a test
+        """
+        self.assertRaisesRegex(TypeError, "object of type *", max_integer, 81)
 
 
 if __name__ == '__main__':
