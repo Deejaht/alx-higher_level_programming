20a1968ff0f335cd221cfa9b9bacae262f5d51dd
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-07-07 17:49:24 +0100

    update

--- others/hashtables/0-hash_table_create.c
+++ others/hashtables/0-hash_table_create.c
@@ -8,13 +8,21 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table = NULL;
+	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+	unsigned int i = 0;
 
-	if (!size)
+	if (!size || !new_table)
 		return (NULL);
-	new_table = malloc(sizeof(hash_node_t *) * size);
 
-	if (!new_table)
+	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (!new_table->array)
+	{
+		free(new_table);
 		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		new_table->array[i] = NULL;
 	return (new_table);
 }
