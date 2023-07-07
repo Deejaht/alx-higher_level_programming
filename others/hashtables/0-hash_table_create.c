df9a8273f04cf34e3909ed76a6c0681bb34fd746
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-07-07 15:47:14 +0100

    one

---
+++ others/hashtables/0-hash_table_create.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function name
+ * @size: size of hash table
+ *
+ * Return: hash_table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table = malloc(sizeof(hash_node_t *) * size);
+
+	if (!new_table)
+	{
+		return (NULL);
+	}
+	return (new_table);
+}
