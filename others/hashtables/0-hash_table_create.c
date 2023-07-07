b632a864d0aef34541033f8a665d881589f7a936
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-07-07 15:48:47 +0100

    one

--- others/hashtables/0-hash_table_create.c
+++ others/hashtables/0-hash_table_create.c
@@ -8,11 +8,13 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table = malloc(sizeof(hash_node_t *) * size);
+	hash_table_t *new_table = NULL;
+
+	if (!size)
+		return (NULL);
+	new_table = malloc(sizeof(hash_node_t *) * size);
 
 	if (!new_table)
-	{
 		return (NULL);
-	}
 	return (new_table);
 }
